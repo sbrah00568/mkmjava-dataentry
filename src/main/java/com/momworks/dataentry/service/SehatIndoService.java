ab1f111b20fbee08d6dde38ccef718b9793f3090
sbrah00568 <harbisnugroho@gmail.com>
2024-10-20 17:39:24 +0700

    feat(sehat-indonesiaku): Automate value setting for set pos imunisasi before mulai aktivitas in imunisasi rutin

--- src/main/java/com/momworks/dataentry/service/SehatIndoService.java
+++ src/main/java/com/momworks/dataentry/service/SehatIndoService.java
@@ -1,5 +1,7 @@
 package com.momworks.dataentry.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.momworks.dataentry.configuration.SehatIndoProperties;
 import com.momworks.dataentry.dto.SehatIndoDto;
 import com.momworks.dataentry.dto.SehatIndoImunisasiDto;
@@ -15,6 +17,11 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.PointerInput;
@@ -27,10 +34,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.net.URL;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 @Slf4j
@@ -47,6 +51,7 @@ public class SehatIndoService {
     private static final String POINT_ZERO = ".0";
     private static final String EMPTY_STRING = "";
     private static final String SPACE = " ";
+    private static final String HYPHEN = "-";
 
     public void execute(MultipartFile xlsxFile, String type) {
         AppiumDriver driver = null;
@@ -224,7 +229,8 @@ public class SehatIndoService {
                                 "/android.view.View/android.view.View/android.view.View//android.widget.ImageView[@content-desc=\"Imunisasi Rutin\"]")));
                 imunisasiRutinButton.click();
 
-                // Start input tanggal imunisasi rutin (open tanggal seekbar)
+                // TODO: handle logic to choose datetime. Must not on holiday (Saturday when not public holiday is ok)
+                // ================== START input tanggal imunisasi rutin (open tanggal seekbar) ===================================================================================================
                 WebElement imunisasiRutinTanggalButton = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(
                         "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout" +
                                 "/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.widget.ImageView")));
@@ -271,6 +277,73 @@ public class SehatIndoService {
                 tapOkeButton.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
                 driver.perform(List.of(tapOkeButton));
                 // ========= END performing tap Oke button on tanggalSeekbar action to close tanggal seekbar and set tanggal imunisasi ========= //
+
+                // ================== END input tanggal imunisasi rutin (open tanggal seekbar) ===================================================================================================
+
+                // ================== START input pos imunisasi rutin ===================================================================================================
+                WebElement imunisasiRutinPosButton = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+                        "//android.widget.EditText")));
+                imunisasiRutinPosButton.click();
+
+                WebElement searchPosImunisasiButton = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+                        "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout" +
+                                "/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.ImageView[2]")));
+                searchPosImunisasiButton.click();
+
+                String pos = EMPTY_STRING;
+                for (Map.Entry<String, SehatIndoImunisasiDto> detailImunisasi : sehatIndoDto.getDetailImunisasiMap().entrySet()) {
+                    if (!Objects.equals(detailImunisasi.getValue().getPos(), HYPHEN)) {
+                        pos = detailImunisasi.getValue().getPos().replace("PYD", EMPTY_STRING).replace("POSYANDU", EMPTY_STRING).replace("-WANASARI", EMPTY_STRING);
+                    }
+                }
+                pos = Objects.equals(pos, EMPTY_STRING) ? "DALAM GEDUNG" : pos;
+
+                WebElement cariDisiniClickPosImunisasi = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+                        "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout" +
+                                "/android.view.View/android.view.View/android.view.View/android.widget.ImageView[2]")));
+                cariDisiniClickPosImunisasi.click();
+
+                WebElement cariDisiniSendKeysPosImunisasi = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+                        "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout" +
+                                "/android.view.View/android.view.View/android.view.View/android.widget.ImageView[2]")));
+                cariDisiniSendKeysPosImunisasi.sendKeys(pos);
+
+                WebElement startCariDisiniPosImunisasi = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+                        "//android.view.View[@content-desc=\"Cari pos imunisasi\"]")));
+                startCariDisiniPosImunisasi.click();
+
+                webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+                        "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout" +
+                                "/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View")));
+
+                String hasilPencarianXmlHierarchy = driver.getPageSource();
+
+                Document hasilPencarianDoc = Jsoup.parse(hasilPencarianXmlHierarchy, Parser.xmlParser());
+
+                Elements hasilPencarianElementList = hasilPencarianDoc.getAllElements();
+
+
+                // TODO: handle when hasil tidak ditemukan, must roll back to previous action to search "DALAM GEDUNG" for default search
+                String hasilPencarian = EMPTY_STRING;
+                for (Element hasilPencarianElement : hasilPencarianElementList) {
+                    String contentDesc = hasilPencarianElement.attr("content-desc");
+                    if (!Objects.equals(contentDesc, EMPTY_STRING) && !Objects.equals(contentDesc, "Pilih") && !Objects.equals(contentDesc, "Hasil pencarian") && contentDesc.contains("WANASARI")) {
+                        hasilPencarian = contentDesc;
+                        break;
+                    }
+                }
+
+                WebElement selectHasilPencarian = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+                        "//android.view.View[@content-desc=\"" + hasilPencarian + "\"]")));
+                selectHasilPencarian.click();
+
+                WebElement pilihHasilPencarian = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+                        "//android.view.View[@content-desc=\"Pilih\"]")));
+                pilihHasilPencarian.click();
+
+                log.info("");
+
+                // ================== END input pos imunisasi rutin ===================================================================================================
 
                 // Exit imunisasi rutin menu
                 WebElement exitImunisasiRutinMenu = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(
